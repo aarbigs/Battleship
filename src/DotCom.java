bb1cf4afaeac4b4d1276351f507abde0f248e735
Aaron <bigelow_aaron@hotmail.com>
2017-10-13 19:47:59 -0600

    initial commit

---
+++ src/DotCom.java
@@ -0,0 +1,34 @@
+/**
+ * Created by aaron on 10/13/17.
+ */
+import java.lang.reflect.Array;
+import java. util.*;
+
+public class DotCom {
+
+    private ArrayList<String> locationCells;
+    private String name;
+
+    public void setLocationCells(ArrayList<String> loc) {
+        locationCells = loc;
+    }
+
+    public void setName(String n) {
+        name = n;
+    }
+
+    public String checkYourself(String userInput) {
+        String result = "miss";
+        int index = locationCells.indexOf(userInput);
+        if (index >= 0) {
+            locationCells.remove(index);
+            if (locationCells.isEmpty()){
+                result = "kill";
+                System.out.println("Ouch you sunk " + name + " : ( ");
+            } else {
+                result = "hit";
+            }
+        }
+        return result;
+    }
+}
